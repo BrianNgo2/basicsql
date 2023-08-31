5a8a600fff377025f49b165b6a18329db81c1173
BrianNgo2 <122535751+BrianNgo2@users.noreply.github.com>
2023-08-31 18:05:11 +0700

    Add files via upload

---
+++ Join-SeflJoin-CTE.sql
@@ -0,0 +1,184 @@
+﻿ /* From dbo.DimProduct, dbo.DimPromotion, dbo.FactInternetSales,  
+
+Write a query display ProductKey, EnglishProductName which has Discount Pct >= 20%*/ 
+
+-- Your code here  
+
+SELECT  
+
+P.Productkey 
+
+, P.EnglishProductName 
+
+, DPm.DiscountPct 
+
+, FORMAT(DPm.DiscountPct, 'p' )  AS PCT   
+
+From dbo.DimProduct AS P  
+
+LEFT JOIN dbo.FactInternetSales AS FIS ON FIS.ProductKey=P.ProductKey 
+
+LEFT JOIN dbo.DimPromotion AS DPm ON DPm.PromotionKey=FIS.PromotionKey   
+
+Where DPm.DiscountPct  >= 0.2 
+
+---Bạn nên lấy bảng FactInternetSales làm chính nhé. để không bị mất mát dữ liệu giao dịch nhé 
+
+ 
+
+/*From dbo.DimProduct and DimProductSubcategory, DimProductCategrory 
+
+Write a query displaying the Product key, EnglishProductName, EnglishProductSubCategoryName , EnglishProductCategroyName columns of product which has EnglishProductCategoryName is 'Clothing'*/ 
+
+-- Your code here  
+
+SELECT  
+
+P.ProductKey 
+
+, P.EnglishProductName 
+
+, DPS.EnglishProductSubcategoryName 
+
+, DPC.EnglishProductCategoryName 
+
+FROM Dbo.DimProduct AS P  
+
+LEFT JOIN dbo.DimProductSubcategory AS DPS ON P.ProductSubcategoryKey=DPS.ProductSubcategoryKey 
+
+LEFT JOIN dbo.DimProductCategory AS DPC ON DPS.ProductCategoryKey=DPC.ProductCategoryKey 
+
+WHERE DPC.EnglishProductCategoryName = 'Clothing' 
+
+ 
+
+--- Bailàm đúng 
+
+ 
+ /*
+From FactInternetSales, DimProduct  
+Display ProductKey, EnglishProductName, ListPrice of products which never have been sold  
+*/
+-- Your code here  
+
+SELECT  
+
+ProductKey 
+
+, EnglishProductName 
+
+, ListPrice 
+
+FROM Dbo.DimProduct 
+
+WHERE ProductKey NOT IN (SELECT ProductKey FROM Dbo.FactInternetSales)  
+
+ 
+
+ 
+
+-- Bài làm đúng 
+
+/*
+
+From DimDepartmentGroup, Write a query display DepartmentGroupName and 
+
+their parent DepartmentGroupName  */
+
+-- Your code here  
+
+SELECT  
+
+DDG.DepartmentGroupName 
+
+, PDDG.DepartmentGroupName AS ParentDepartmentGroupName 
+
+FROM dbo.DimDepartmentGroup AS DDG 
+
+LEFT JOIN dbo.DimDepartmentGroup AS PDDG ON DDG.ParentDepartmentGroupKey=PDDG.DepartmentGroupKey 
+
+ 
+
+-- Bài làm đúng 
+
+ 
+
+/*From FactFinance, DimOrganization, DimScenario 
+
+Write a query display OrganizationKey, OrganizationName, Parent OrganizationKey, Parent OrganizationName, Amount 
+
+where ScenarioName is 'Actual'  
+
+-- Your code here*/  
+
+SELECT  
+
+FFjDS.OrganizationKey 
+
+, DOjPDO.OrganizationName 
+
+, DOjPDO.ParentOrganizationKey 
+
+, DOjPDO.ParentOrganizationName 
+
+, FFjDS.Amount 
+
+FROM 
+
+(SELECT  
+
+FF.OrganizationKey 
+
+, FF.Amount 
+
+, DS.ScenarioName 
+
+FROM dbo.FactFinance AS FF 
+
+LEFT JOIN dbo.DimScenario AS DS ON FF.ScenarioKey=DS.ScenarioKey 
+
+WHERE ScenarioName = 'Actual') AS FFjDS 
+
+LEFT JOIN ( 
+
+SELECT 
+
+DO.OrganizationKey, 
+
+DO.OrganizationName, 
+
+DO.ParentOrganizationKey, 
+
+PDO.OrganizationName AS ParentOrganizationName 
+
+FROM dbo.DimOrganization AS DO  
+
+LEFT JOIN dbo.DimOrganization AS PDO ON DO.ParentOrganizationKey=PDO.OrganizationKey ) AS DOjPDO ON FFjDS.OrganizationKey=DOjPDO.OrganizationKey 
+
+ 
+
+-- Bài làm đúng, nhưng bạn có thể viết tuần tự các JOIN là được nhé 
+---CTE: 
+/*
+WITH temp_sale AS (  
+
+    SELECT  
+
+        ProductKey  
+
+    FROM dbo.FactInternetSales  
+
+    WHERE SalesAmount > 1000 )  
+
+SELECT  
+
+    ProductKey,  
+
+    EnglishProductName,  
+
+    Color  
+
+FROM dbo.DimProduct  
+
+WHERE ProductKey IN (SELECT ProductKey FROM temp_sale)
+*/
---
+++ Where.sql
@@ -0,0 +1,171 @@
+﻿-- WHERE clause 
+
+/* Ex1. (slide) From table DimEmployee, select all records that satisfy one of the following conditions: 
+
+• DepartmentName is equal to 'Tool Design 
+
+• Status does NOT include the value NULL 
+
+• StartDate in the period from '2009-01-01' to '2009-12-31' 
+
+And must have VacationHours > 10 */ 
+
+-- Your code here 
+
+use AdventureWorksDW2019 
+
+go 
+
+Select *  
+
+From dbo.Dimemployee  
+
+Where 
+
+[VacationHours] > 10 and 
+
+ [DepartmentName]='Tool Design'  
+
+or [Status] IS NOT NULL  
+
+or [StartDate] between '2009/01/01' and '2009/12/31'  
+--- Bài làm chưa đúng , thiếu điều kiện VacationHour > 10 
+
+ 
+
+/*Ex2. From DimProduct display ProductKey, ProductAlternateKey and EnglishProductName of products  
+
+which have ProductAlternateKey begins with 'BK-' followed by any character other than 'T' and ends with a '-' followed by any two numerals.  
+
+And satisfy Color are black, red, or white  */
+
+-- Your code here 
+
+SELECT 
+
+ProductKey 
+
+, ProductAlternateKey 
+
+,EnglishProductName  
+
+FROM dbo.DimProduct 
+
+Where ProductAlternateKey like 'BK-[^T]%' and  
+
+ProductAlternateKey like '%-[0-9][0-9]' 
+
+and [Color] in ('Black', 'Red', 'White') 
+
+--- Bài làm chưa đúng phần thể hiện kí tự khác T 
+
+ 
+
+ 
+
+/*Ex3: From FactInternetSales, get a the records which ordered from '2011-01-01' and shipped in 2011 */  
+
+-- Your code here 
+
+SELECT * 
+
+FROM FactInternetSales 
+
+WHERE [OrderDate] >= '2011-01-01' 
+
+and YEAR([ShipDate]) = '2011' 
+
+-- Data types functions 
+--- Bài làm đúng 
+
+ 
+
+ 
+
+ /* Ex4. From DimEmployee table, get EmployeeKey, then: 
+
+- Generate a new field named 'Full Name' which combined FirstName, MiddleName, LastName columns (by 2 ways: using '+' and functions)  
+
+(Noted that MiddleName might contain NULL) 
+
+- Calculate age of each Employee when they are hired using HireDate, BirthDate columns  
+
+- Calculate age of each Employee today using BirthDate column 
+
+- Get user name of each employee. Username is last part of login ID: adventure-works\jun0 -> Username = jun0 
+
+*/ 
+
+-- Your code here 
+
+SELECT 
+
+EmployeeKey 
+
+---Cach1 function: 
+
+,CONCAT([FirstName],' ',[MiddleName],' ',[LastName]) as [FullName1] 
+
+---Cach 2 + : 
+
+,[FirstName]+' '+ISNULL([MiddleName],'')+' '+[LastName] as [FullName2] 
+
+,DATEDIFF(YEAR,[BirthDate],[HireDate]) AS HiredAge 
+
+,DATEDIFF(YEAR,[BirthDate],GETDATE()) AS Age  
+
+,[LoginID] 
+
+--Cach1 Substring() 
+
+, Substring([LoginID],17,256) as UserNameCach1 
+
+--Cach 2 Stuff() 
+
+,Stuff([LoginID],1,16,'') as UserNameCach2 
+
+--Cach3 Replace() 
+
+,Replace([LoginID],'adventure-works\','') as UserNameCach3 
+
+FROM DimEmployee 
+
+ 
+
+ 
+
+--- Bài làm chưa đúng, yêu cầu tạo column FullName bằng 2 cách\ 
+
+ 
+
+ 
+
+/* Ex5: From DimEmployee get EmployeeKey, Full Name (combine FirstName, MiddleName and LastName) of all employees that have 8th character (from left) in FirstName equal to "a"  
+
+-- Your code here */
+
+SELECT  
+
+EmployeeKey 
+
+,CONCAT([FirstName],' ',[MiddleName],' ',[LastName]) as [FullName] 
+
+FROM DimEmployee 
+
+Where  
+
+---Cach1 stuff() remove 7 kí tự đầu => kí tự 1 (8 trong chuỗi cũ) là a  
+
+---Stuff([FirstName],1,7,'') like 'a%'  
+
+---Cach2 substring() Lấy first name từ kí tự 8 => kí tự đầu chuỗi mới là a 
+
+---SUBSTRING([FirstName],8,256) like 'a%' 
+
+---Cach 3 hàm left() Lấy hàm 8 kí tự trái của first name => có hai trường hợp Th1 tên đủ 8 kí tự thì kết quả đúng Th2 tên không đủ 8 kí tự vẫn trả ra nhiêu tên có kí tự cuối là a => sai cần thêm điều kiện độ dài. 
+
+Len([FirstName]) >= 8 
+
+and left(FirstName,8) like '%a' 
+
+---Bài làm đúng, nhưng nên sử dụng CONCAT_WS, nếu vẫn dùng CONCAT thì cần dùng ISNULL ở MiddleName nhé 
